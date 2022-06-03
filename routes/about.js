eb44b3cff21846a9345d74483a68535981614db7
HugoBzn <hugobazan1499@gmail.com>
2022-06-03 14:09:36 -0500

    Boiler Plate

---
+++ routes/about.js
@@ -0,0 +1,19 @@
+// Importar Express
+var express = require('express');
+// Importamos el enrutador de Express
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //Render manda a renderizar (generar y entregar) ls vista al cliente
+
+  res.render('about', 
+  //Este es el View-Model
+  {
+    name: 'HugoBzn',
+    email: 'hugobazan1499@gmail.com',
+    url: "https://github.com/HugoBzn"
+  });
+});
+
+module.exports = router;
