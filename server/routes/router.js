b120aabbe4c75c8b0a0d2145760cb45c665fcc9a
HugoBzn <hugobazan1499@gmail.com>
2022-06-03 21:55:54 -0500

    S42 Agregando rutas de admin

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,15 @@
 // Importando el enrutador de Home
 import homeRouter from './homeRouter';
 
+// Importando el enrutador de admin
+import adminRouter from './adminRouter';
+
 // Funcion que agrega todos los enrutadores a la aplicacion de express
 const addRoutes = (app) => {
   /* Agregando el enrutador a Home */
   app.use('/', homeRouter);
+  /* Agregando al enrutador Project */
+  app.use('/admin', adminRouter);
 };
 
 export default {
