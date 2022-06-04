c74332b80821ccc636b6d78a0a718af0d98d7652
HugoBzn <hugobazan1499@gmail.com>
2022-06-03 20:45:02 -0500

    S36 About Route

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -26,7 +26,17 @@ const index = (req, res) => {
   res.render('index', viewModel);
 };
 
+// URL: Get /about
+const about = (req, res) => {
+  res.render('home/aboutView', {
+    name: 'HugoBzn',
+    email: 'hugobazan14@hotmail.com',
+    url: 'https://github.com/HugoBzn',
+  });
+};
+
 export default {
   // Action Methods
   index,
+  about,
 };
